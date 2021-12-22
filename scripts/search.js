ad8f669178ede0b69fa0746a1ac0a95fcfd0f8c3
Rossan P <53863541+rossanp@users.noreply.github.com>
2021-12-22 14:49:12 -0300

    Implementação de micro interações

--- scripts/search.js
+++ scripts/search.js
@@ -4,19 +4,20 @@ pesquisa.addEventListener('click', function () {
 
     let inpt = document.getElementById("search");
     let pesquisa = document.getElementById('title-img')
-    let menuHamb = document.getElementById('container-check')
+    let logo = document.getElementById('logo')
     let header = document.getElementById('header')
     
     /* Modificando o CSS */
     if (inpt.style.display === 'none') {
         header.style.gridTemplateColumns = '45% 40% 15%'
         pesquisa.style.display = 'block'
-        menuHamb.style.display = 'block'
+        logo.style.display = 'block'
         inpt.style.display = 'none'
     } else {
-        header.style.gridTemplateColumns = '45% 55%'
+        header.style.gridTemplateColumns = '0% 85% 15%'
         pesquisa.style.display = 'none'
-        menuHamb.style.display = 'none'
+        logo.style.display = 'none'
         inpt.style.display = 'block'
+        inpt.style.margin = '0'
     }
 })
