f9f8ce306c84463f3711cee7640144c48d7beff1
Rossan P <53863541+rossanp@users.noreply.github.com>
2022-01-04 14:43:40 -0300

    Tentativa para aplicar Highlight

--- scripts/imprimiCards.js
+++ scripts/imprimiCards.js
@@ -5,6 +5,8 @@ function imprimiCard() {
     let card = ""
 
     for (i = 0; i < dadosCard.length; i++) {
+        let language = dadosCard[i].linguagen
+
         card += `<div class="card__edit">`
         card += `<div class="card__fundo-codigo" style="background-color:${dadosCard[i].cor}">`
         card += `<div class="card__container-cod">`
@@ -14,7 +16,7 @@ function imprimiCard() {
         card += `<div class="circle3"></div>`
         card += `</div>`
         card += `<div class="card__inpt-dev">`
-        card += `<code spellcheck="false">`
+        card += `<code class="preview hljs ${language}" contenteditable="true" aria-label="Editor de código">`
         card += dadosCard[i].cod
         card += `</code>`
         card += `</div>`
@@ -52,13 +54,9 @@ function imprimiCard() {
         card += `</div>`
         card += `</div>`
 
-
-        /* console.log(`O item 5 do array é ${dados[i].id} e ${dados[i].nome}.`) */
     }
 
     imprimi.innerHTML = card
 }
-
-/* imprimiCard() */
 
 window.addEventListener("load", imprimiCard)
